07e01f4e80adad6307acf3232b1b9ad79120ac0d
carey <727929807@qq.com>
2018-05-16 21:11:07 +0800

    项目架构

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request) {
+        return "index";
+    }
+}
